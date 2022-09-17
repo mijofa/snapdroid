4348aa0bfd575090f90f09442d4fca2d3c26294c
Kostas Chatzikokolakis <kostas@chatzi.org>
2022-09-17 12:06:36 +0200

    Start/stop SnapclientService via broadcast intents

--- Snapcast/src/main/java/de/badaix/snapcast/BroadcastReceiver.java
+++ Snapcast/src/main/java/de/badaix/snapcast/BroadcastReceiver.java
@@ -18,41 +18,58 @@
 
 package de.badaix.snapcast;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.text.TextUtils;
+import android.util.Log;
 
 import de.badaix.snapcast.utils.Settings;
 
 /**
  * Created by johannes on 05.05.16.
  */
-public class BootCompleteReceiver extends BroadcastReceiver {
+public class BroadcastReceiver extends android.content.BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
-            if (Settings.getInstance(context).isAutostart()) {
-                String host = Settings.getInstance(context).getHost();
-                int port = Settings.getInstance(context).getStreamPort();
-                if (TextUtils.isEmpty(host))
-                    return;
-
-                Intent i = new Intent(context, SnapclientService.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                i.putExtra(SnapclientService.EXTRA_HOST, host);
-                i.putExtra(SnapclientService.EXTRA_PORT, port);
-                i.setAction(SnapclientService.ACTION_START);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    context.startForegroundService(i);
-                } else {
-                    context.startService(i);
+        switch (intent.getAction()) {
+            // Auto start snapclient on boot
+            case "android.intent.action.BOOT_COMPLETED":
+                if (Settings.getInstance(context).isAutostart()) {
+                    startService(context, SnapclientService.ACTION_START);
                 }
-            }
+                break;
+
+            // Control snapclient service via broadcast intents
+            case "de.badaix.snapcast.START_SERVICE":
+                startService(context, SnapclientService.ACTION_START);
+                break;
+            case "de.badaix.snapcast.STOP_SERVICE":
+                startService(context, SnapclientService.ACTION_STOP);
+                break;
+        }
+    }
+
+    private void startService(Context context, String action) {
+        Intent i = new Intent(context, SnapclientService.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        i.setAction(action);
+
+        if (action == SnapclientService.ACTION_START) {
+            String host = Settings.getInstance(context).getHost();
+            int port = Settings.getInstance(context).getStreamPort();
+            if (TextUtils.isEmpty(host))
+                return;
+
+            i.putExtra(SnapclientService.EXTRA_HOST, host);
+            i.putExtra(SnapclientService.EXTRA_PORT, port);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(i);
+        } else {
+            context.startService(i);
         }
     }
 }
-
 
